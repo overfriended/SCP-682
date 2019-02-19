8100daf25ac76d256dcce53e21bc94a8208677f0
VRPCaden <giantballofcrap@gmail.com>
2019-02-19 12:23:39 -0400

    Add project files.

---
+++ SCP-008/Events/PlayerEvents.cs
@@ -0,0 +1,13 @@
+﻿using Smod2.EventHandlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP_008
+{
+    class PlayerEvents : IEventHandlerPlayerDie
+    {
+    }
+}
---
+++ SCP-008/SCP682.cs
@@ -0,0 +1,27 @@
+﻿using Smod2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP_682
+{
+    public class SCP682 : Plugin
+    {
+        public override void OnDisable()
+        {
+            
+        }
+
+        public override void OnEnable()
+        {
+
+        }
+
+        public override void Register()
+        {
+
+        }
+    }
+}
